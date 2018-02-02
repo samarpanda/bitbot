3e9887fdb6d27e450ed5b5ca69d3fbda2eebade3
Samar Panda <er.samarpanda@gmail.com>
2018-02-02 21:57:10 +0530

    wit api integrated

--- server/api/demo/demoRoutes.js
+++ server/api/demo/demoRoutes.js
@@ -2,16 +2,8 @@ var router = require('express').Router()
 var logger = require('../../util/logger')
 var controller = require('./demoController')
 
-// router.param('id', controller.params)
-// router.get('/me', checkUser, controller.me)
-
 router.route('/')
   .get(controller.get)
-  // .post(controller.post)
-
-// router.route('/:id')
-//   .get(controller.getOne)
-//   .put(checkUser, controller.put)
-//   .delete(checkUser, controller.delete)
+  .post(controller.post)
 
 module.exports = router
