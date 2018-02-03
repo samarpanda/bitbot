0951f0a7f8908401222fb4f60d560dd07debbf7e
Samar Panda <er.samarpanda@gmail.com>
2018-02-03 13:24:49 +0530

    updated

--- server/api/demo/demoController.js
+++ server/api/demo/demoController.js
@@ -20,19 +20,23 @@ exports.get = function(req, res, next){
 }
 
 exports.getPopularAds = function(req, res, next){
-
-  auth.getAccessToken()
-  .then((authRes) => {
-    return auth.getHeaders(authRes.body)
+  const headerObj = {
+"X-QUIKR-CLIENT-SIGNATURE":"zXcv80386Mdp1hs0q7o0p9uiLZV37TdF",
+"X-QUIKR-CLIENT-VERSION":"9.25",
+"Content-Type":"application/json",
+"X-QUIKR-CLIENT-DEMAIL":"qae6ficmqr26y@quikr.com",
+"X-Quikr-Client":"Android.9_25",
+"X-Quikr-Token-Id":"790798480",
+"X-Quikr-Signature-V2":"0657d0a548d65863d9a4e021ea02e4fd07a9d4b1",
+"X-Quikr-App-Id":"912"
+}
+// console.log(headerObj);
+qapi.getPopularAds(headerObj)
+  .then((ares) => {
+    // console.log(ares)
+    res.json(JSON.parse(ares))
   })
-  .then((obj) => {
-    qapi.getPopularAds(obj)
-      .then((ares) => {
-        // console.log(JSON.stringify(ares.text))
-        res.json(ares.text)
-      })
-      .catch((err) => {
-        logger.error(JSON.stringify(err))
-      })
+  .catch((err) => {
+    logger.error(JSON.stringify(err))
   })
 }
