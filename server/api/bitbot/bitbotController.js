0556e286340bf282066ed47bf6279e3a924eb568
Samar Panda <er.samarpanda@gmail.com>
2018-02-03 16:49:57 +0530

    error handling for facebook notifications

--- server/api/bitbot/bitbotController.js
+++ server/api/bitbot/bitbotController.js
@@ -8,6 +8,16 @@ var auth = require('../../q/auth')
 
 const token = "EAAH8lee0ZBnMBACe8ry6inf009p99ZADepqO6kLqZC6NAxBKk7NhM9B52r9i2CISqwF4L8CSqASolK86AePF8aMJgKPIslt1fR9M1DSHydH2hcUAHLJ5SlqRBzrRt49rscStk9D0e8JUYK8imzVwW2fkjJuFtKde6bHHGiLiAZDZD"
 
+const constantPayload = {
+  "attachment":{
+    "type":"template",
+    "payload":{
+      "template_type":"generic",
+      "elements":[]
+    }
+  }
+}
+
 exports.get = function(req, res){
   if (req.query['hub.verify_token'] === "ujjwal") {
     res.status(200).send(req.query['hub.challenge'])
@@ -56,9 +66,12 @@ function wapiCall(text, sender){
               search.search(wres._text)
                   .then((dres) => {
                     let rest = JSON.parse(dres.text)
-                    let respo = rest.SearchApplicationResponse.SearchApplication.ads;
+                    let respo = rest.SearchApplicationResponse.SearchApplication.ads || [];
                     console.log("$$$$ response form search " + util.inspect(wres, {depth: null}))
-                    sendText(respo, sender)
+                    if(respo.length)
+                      sendText(respo, sender)
+                    else
+                      logger.debug("No ads found")
                   })
                   .catch((err) => {
                     logger.error(err)
@@ -146,29 +159,12 @@ function sendText(ads, sender){
     responsePayload.attachment.payload.elements.push(addValues(title, image_url))
   })
   //console.log("$$$$ response at end " + util.inspect(responsePayload, {depth: null}))
-  sendResponse(sender, responsePayload)
-  // request
-  //   .post('https://graph.facebook.com/v2.6/me/messages')
-  //   .set('content-type', 'application/json')
-  //   .query({access_token: token})
-  //   .send({
-  //     recipient: {id: sender},
-  //     message: responsePayload
-  //   })
-  //   .then(function(res){
-  //     console.log("$$$$ final response " + util.inspect(res, {depth: null}))
-  //   })
+  if(responsePayload.attachment.payload.elements.length)
+    sendResponse(sender, responsePayload)
+  else
+    logger.debug("No respond to be sent")
 }
 
-const constantPayload = {
-  "attachment":{
-    "type":"template",
-    "payload":{
-      "template_type":"generic",
-      "elements":[]
-    }
-  }
-}
 
 function addValues(title, image_url){
   var str = {
